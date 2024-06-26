8d41069c624f1f93f42cdf9079a6bdd37dfbf943
Karl Cardenas <karl@spectrocloud.com>
2024-06-26 10:28:02 -0700

    ci: more debug

--- scripts/check_rule.sh
+++ scripts/check_rule.sh
@@ -87,10 +87,7 @@ check_fail_conditions() {
 
   RESULT=$(vale --config="$config_file" --no-exit --output=line  "$fail_md_file" |  wc -l)
 
-    # Debugging: Print the RESULT value
-    echo "RESULT: $RESULT"
-
-  if [ "$RESULT" -eq 0 ]; then
+  if [ -z $RESULT ]; then
       echo "$package_name/$rule_name fail condition test - ❌"
       log=$(vale --config="$config_file" --output=line "$fail_md_file")
       echo "Debug: $log"
